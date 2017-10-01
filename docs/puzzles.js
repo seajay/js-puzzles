3c9ea01723074f06006c08ba62f1639a31ae51d8
Chris Jones <aws@cjones.eu>
2017-10-01 15:10:02 +0100

    Working (using local puzzle)

--- docs/puzzles.js
+++ docs/puzzles.js
@@ -1,3 +1,42 @@
+var output;  // The value of a textarea where the test can put its output
+var success; // true if the test passed
+var userCode; // The value of the textarea where the user code is entered.
+var instructions; // The innerHTML of the instructions div
+
+var currentPuzzle;
+
+var testPuzzle = {
+	"title": "01 Commenting",
+	"level": 1,
+	"description": "Comment out the line 'success = false;'",
+	"template": "success = true;\nsuccess = false;",
+	"test": "output.value = success?'Test passed':'Test failed';",
+	"solution": "success = true;\n//success = false;"
+};
+
+window.onload = function (){
+	output = document.getElementById("outputText");
+	userCode = document.getElementById("userCode");
+	instructions = document.getElementById("instructions");
+	
+	loadPuzzle(testPuzzle);
+}
+
+function loadPuzzle(puzzle){
+	currentPuzzle = puzzle;
+	output.value = "";
+	success = false;
+	userCode.value = puzzle.template;
+	instructions.innerHTML = puzzle.description;
+}
+	
+
 function runTest(){
-		alert("runTest");
-	}
+		eval(userCode.value);
+		eval(currentPuzzle.test);
+}
+
+function cheat(){
+	userCode.value = currentPuzzle.solution;
+	runTest();
+}
